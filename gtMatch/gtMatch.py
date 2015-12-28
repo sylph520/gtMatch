56768ef898b98124dd806874d0e1a243719ac0c2
pallas <pallasjack@outlook.com>
2015-12-28 16:54:31 +0800

    coarse description of program steps

---
+++ gtMatch/gtMatch.py
@@ -0,0 +1,5 @@
+""" This project is for the geometry element extraction, and then testify the results of it."""
+### coarse description of steps
+# 1. import question text
+# 2. start processing text
+# 3. start compare the result and testify the program
